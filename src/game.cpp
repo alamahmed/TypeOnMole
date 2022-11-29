c03663187acda550603379ccb70dfa13ed8b277b
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.local>
2022-11-29 18:47:03 +0500

    Adding level reading Functionality

--- src/game.cpp
+++ src/game.cpp
@@ -1,20 +1,31 @@
 #include "game.h"
 #include <string>
 #include <raylib.h>
-#include <assert.h>
+// #include <assert.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+using std::cout;
+using std::to_string;
+using std::string;
+using std::ifstream;
+using std::vector;
+using std::copy;
+using std::back_inserter;
 
 // g++ Game.cpp -std=c++20 `pkg-config --libs --cflags raylib` -o Game
 
-Game::Game(int width, int height, int fps, std::string title)
+Game::Game(int width, int height, int fps, string title)
 {
-    assert(!GetWindowHandle());               // If assertion Triggers : Window is already opened
+    // assert(!GetWindowHandle());               // If assertion Triggers : Window is already opened
     SetTargetFPS(fps);
     InitWindow(width, height, title.c_str());
+    readLvl("level.txt");
 }
 
 Game::~Game() noexcept
 {
-    assert(GetWindowHandle());               // If assertion Triggers : Window is already Closed
+    // assert(GetWindowHandle());               // If assertion Triggers : Window is already Closed
     CloseWindow();
 }
 
@@ -35,10 +46,58 @@ void Game::Draw()
 {
     ClearBackground(BLACK);
     DrawFPS(10, 20);
-    DrawCircle(GetMousePosition().x, GetMousePosition().y, 30 ,RED);
+    // DrawCircle(GetMousePosition().x, GetMousePosition().y, 30 ,RED);
+    renderLevel();
 }
 
 void Game::Update()
 {
+    // renderLevel();
+}
 
+void Game::readLvl(string levelName)
+{
+    ifstream level("src/" + levelName);
+    vector<vector<char> > lvl;
+    string line;
+    if(!level)
+        cout << "Failed to read file " << levelName << "\n";
+    while (getline(level, line))
+    {
+        vector <char> temp;
+
+        copy(line.begin(), line.end(), back_inserter(temp));
+
+        lvl.push_back(temp);
+    }
+    currentLevel = lvl;
+}
+
+void Game::renderLevel()
+{
+    // -------------------------------------------------------------------------------------
+    ClearBackground(RAYWHITE);
+    // ADDING BACKGROUND COLOR
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), RAYWHITE);
+    // --------------------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------------------
+    // Draw Text
+    int levelNum = 1;
+    string levelName = "LEVEL " + to_string(levelNum);
+    DrawText(levelName.c_str(), 0, 0, 50, BLACK);
+    // --------------------------------------------------------------------------------------
+    
+    // --------------------------------------------------------------------------------------
+    // DRAW LEVEL
+    for(int i = 0; i < int(currentLevel.size()); i++)
+    {
+        for(int j = 0; j < int(currentLevel.at(i).size()); j++)
+        {
+            if(currentLevel.at(i).at(j) == '1')
+                DrawCircle(i * 30 + 350, j * 30 + 200, 15, BLACK);
+            else
+                DrawCircle(i * 30 + 350, j * 30 + 200, 15, RED);
+        }
+    }
 }
--- src/game.h
+++ src/game.h
@@ -1,15 +1,23 @@
 #include <string>
+#include <vector>
+using std::string;
+using std::vector;
 class Game
 {
     public:
-    Game(int width, int height, int fps, std::string title);
+    Game(int width, int height, int fps, string title);
     ~Game() noexcept;
     Game(const Game& other) = delete;
     Game& operator=(const Game& other) = delete;
     bool GameShouldClose() const;
     void Tick();
+    // vector<vector<char>> readLvl(string levelName);
+    // void renderLevel(vector<vector<char> > level);
+    void readLvl(string levelName);
+    void renderLevel();
 
     private:
+    vector<vector<char> > currentLevel;
     void Update();
     void Draw();
 
